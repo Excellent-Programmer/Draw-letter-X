6c8b580e8a299b435148dce411333b073aae1357
sameh <39744797+sameh20130@users.noreply.github.com>
2020-12-23 19:18:59 +0200

    Add files via upload

---
+++ letter X/letter X/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace letter_X
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row, column;
+
+ 
+            for (row = 0; row <= 6; row++)
+            {
+                for (column = 0; column <= 6; column++)
+                {
+                    if (((column == 1 || column == 5) && (row > 4 || row < 2)) || row == column && column > 0 && column < 6 || (column == 2 && row == 4) || (column == 4 && row == 2))
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+            Console.Read();
+        }
+    }
+}
